07e77f644af1c3bc36bc2c788f60aed22626d0b4
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-02-20 15:32:07 +0900

    Solved 2 problems

---
+++ level-2/최댓값과-최솟값.js
@@ -0,0 +1,17 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//완벽한 정답이 아닙니다.
+//정답 1
+function solution(s) {
+    var answer = '';
+    const arr = s.split(' ')
+    answer = Math.min(...arr) + ' ' + Math.max(...arr)
+    return answer;
+}
+
+//정답 2
+function solution(s) {
+    var answer = '';
+    const arr = s.split(' ').map(x=>parseInt(x,10)).sort((a,b)=>a-b)
+    answer = arr[0] + ' ' + arr[arr.length-1]
+    return answer;
+}
