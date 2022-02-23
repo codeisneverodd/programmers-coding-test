a92dba57471cb3a8d9568f3fbc56c1fbfeb5917e
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-02-23 19:30:43 +0900

    Solved 3 problems

---
+++ level-2/124-나라의-숫자.js
@@ -0,0 +1,29 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//완벽한 정답이 아닙니다.
+//정답 1
+function solution(n) {
+    var answer = '';
+    const oneTwoFour = ['4','1','2']
+    while (n>0){
+        const remainder = n % 3
+        answer = oneTwoFour[remainder] + answer;
+        n = Math.floor((n-1)/3) //나누어떨어지지 않을 때에도 -1해도 상관없음
+    }
+    return answer;
+}
+
+//정답 2
+function solution(n) {
+    var answer = '';
+    const oneTwoFour = ['4','1','2']
+    while (n>0){
+        const remainder = n % 3
+        answer = oneTwoFour[remainder] + answer;
+        if(remainder === 0){
+            n = Math.floor((n-1)/3)
+        }else{
+            n = Math.floor(n/3)
+        }
+    }
+    return answer;
+}
