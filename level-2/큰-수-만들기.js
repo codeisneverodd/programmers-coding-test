78e12dc9cc92c9e8408ae21af96643b0f6c5e3fa
gyeong-hyeon-kim <yogurt21433@gmail.com>
2022-03-04 11:51:52 +0900

    Solved 2 problems

---
+++ level-2/큰-수-만들기.js
@@ -0,0 +1,16 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//완벽한 정답이 아닙니다.
+function solution(number, k) {
+    var answer = '';
+    let answerStack = [0]
+    let deleteCount = -1
+    for(let i=0; i<number.length; i++){
+        while(deleteCount < k && number[i] > answerStack[answerStack.length-1]) {
+            answerStack.pop()
+            deleteCount++
+        }
+        if(answerStack.length < number.length - k) answerStack.push(number[i])
+    }
+    answer = answerStack.join('')
+    return answer;
+}
