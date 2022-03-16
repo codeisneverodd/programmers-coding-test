351692b10517f80c44c0b6708a6f1b356cc5a257
codeisneverodd <codeisneverodd@gmail.com>
2022-03-17 08:09:57 +0900

    Add 00-해답-예시.js

---
+++ level-1/00-해답-예시.js
@@ -0,0 +1,11 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//완벽한 정답이 아닙니다.
+//정답 1 - 본인의 깃허브 아이디
+function solution(n) {
+    //프로그래머스에 제출하여 통과된 함수를 복사 붙여넣기 해주세요!
+}
+
+//정답 2 - 본인의 깃허브 아이디
+function solution(n) {
+    //정답을 다른 방법으로도 작성했다면 추가해 주세요!
+}
