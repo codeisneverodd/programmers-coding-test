7ce8b59c79032684260382850519ed21cb4743d9
yongchanson <syc9430@gmail.com>
2022-03-31 13:15:22 +0900

    부족한-금액-계산하기 - 솔루션 추가

--- level-1/부족한-금액-계산하기.js
+++ level-1/부족한-금액-계산하기.js
@@ -8,3 +8,20 @@ function solution(price, money, count) {
     answer = totalCost <= money ? 0 : totalCost - money
     return answer;
 }
+
+//정답 2 - yongchanson
+function solution(price, money, count) {
+  var answer = 0;
+  let sum = price;
+
+  for (i = 2; i <= count; i++) {
+    sum += price * i;
+  }
+
+  if (sum <= money) {
+    answer = 0;
+  } else {
+    answer = sum - money;
+  }
+  return answer;
+}
