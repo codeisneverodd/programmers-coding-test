c0791e32ef216cf11a0fbf07ec0e123230d98af0
yongchanson <syc9430@gmail.com>
2022-03-31 13:14:27 +0900

    2016년 - 솔루션 추가

--- level-1/2016년.js
+++ level-1/2016년.js
@@ -12,3 +12,19 @@ function solution(a, b) {
 
     return answer;
 }
+
+//정답 2 - yongchanson
+function solution(a, b) {
+  const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+
+  let sum = b;
+  for (
+    let i = 0;
+    i < a - 1;
+    i++ //ex)5월인 경우 1~4월까지 더해준다.
+  )
+    sum += month[i];
+
+  return week[sum % 7]; //1일이 금요일이므로, 0이면 목요일이 출력되어야 한다.
+}
