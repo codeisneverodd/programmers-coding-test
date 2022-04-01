f70fce89c86e5f41f5bcded6ce984ac0e2c34a90
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-04-01 14:04:07 +0900

    Update 평균-구하기.js

--- level-1/평균-구하기.js
+++ level-1/평균-구하기.js
@@ -14,3 +14,12 @@ function solution(arr) {
     }
     return answer / arr.length;
 }
+
+
+//정답 3 - chaerin-dev
+function solution(arr) {
+  // arr의 모든 요소를 sum에 더힘
+  let sum = arr.reduce((sum, e) => sum + e);
+  // sum을 arr의 요소의 갯수로 나눈 후 반환
+  return sum / arr.length;
+}
