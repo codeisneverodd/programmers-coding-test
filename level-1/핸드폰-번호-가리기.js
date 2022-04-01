b4b3d05c98f138858a65cfd489da28f2c57e478b
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-04-01 14:05:35 +0900

    Update 핸드폰-번호-가리기.js

--- level-1/핸드폰-번호-가리기.js
+++ level-1/핸드폰-번호-가리기.js
@@ -6,3 +6,11 @@ function solution(phone_number) {
     answer = '*'.repeat(phone_number.length - 4) + phone_number.slice(-4)
     return answer;
 }
+
+//정답 2 - chaerin-dev
+function solution(phone_number) {
+  // "*"을 phone_number의 길이에서 4만큼 뺀 횟수만큼 반복한 문자열과
+  // phone_number의 -4인덱스부터 끝까지 추출한 문자열을
+  // 이어붙인 후 반환
+  return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+}
