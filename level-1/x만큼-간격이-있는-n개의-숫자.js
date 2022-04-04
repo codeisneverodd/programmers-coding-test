b15966cc134b74e17674ad6bf5c9220cd736a698
jaewon <jaewon1676@daum.net>
2022-04-04 14:24:54 +0900

    Update x만큼 간격이 있는 n개의 숫자.js

--- level-1/x만큼-간격이-있는-n개의-숫자.js
+++ level-1/x만큼-간격이-있는-n개의-숫자.js
@@ -20,3 +20,13 @@ function solution(x, n) {
   }
   return answer;
 }
+
+//정답 3 - jaewon1676
+function solution(n) {
+  let str = '';
+  for (let i=0; i<n; i++){
+      // 삼항 연산자와 +로 문자열을 붙여주어 추가.
+      (i % 2 == 0 ? str = str + '수' : str = str + '박')
+  }
+  return str;
+}
