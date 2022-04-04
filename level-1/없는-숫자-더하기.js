9616592f68db4243f5ed5a6e0f42de9a2490f190
Bard <prove.ability00@gmail.com>
2022-04-04 21:41:35 +0900

    [풀이추가] 2022-04-04, 1문제, 없는 숫자 더하기

--- level-1/없는-숫자-더하기.js
+++ level-1/없는-숫자-더하기.js
@@ -3,7 +3,7 @@
 //정답 1 - codeisneverodd
 function solution(numbers) {
     var answer = -1;
-    answer = 45 - numbers.reduce((a, b) => a + b)
+    answer = 45 - numbers.reduce((a, b) => a + b);
     return answer;
 }
 
@@ -11,7 +11,7 @@ function solution(numbers) {
 function solution(numbers) {
     var answer = 0;
     for (let i = 0; i < 10; i++) {
-        answer += numbers.includes(i) ? 0 : i
+        answer += numbers.includes(i) ? 0 : i;
     }
     return answer;
 }
@@ -20,22 +20,33 @@ function solution(numbers) {
 function solution(numbers) {
     var answer = -1;
 
-    let arr = [0,1,2,3,4,5,6,7,8,9];
+    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let arr_sum = 0;
     let numbers_sum = 0;
 
-    for(i=0; i<arr.length/2; i++) {
-        if(arr[i] == arr[arr.length -1 -i]) {
-            arr_sum += arr[i]
+    for (i = 0; i < arr.length / 2; i++) {
+        if (arr[i] == arr[arr.length - 1 - i]) {
+            arr_sum += arr[i];
         } else {
-            arr_sum += arr[i] + arr[arr.length -1 -i]
-        }     
+            arr_sum += arr[i] + arr[arr.length - 1 - i];
+        }
     }
 
-    for(i=0; i<numbers.length; i++) {
-        numbers_sum += numbers[i]
+    for (i = 0; i < numbers.length; i++) {
+        numbers_sum += numbers[i];
     }
 
     answer = arr_sum - numbers_sum;
+    return answer;
+}
+
+//정답 3 - prove-ability
+function solution(numbers) {
+    var answer = 0;
+
+    for (let i = 0; i < 10; i++) {
+        if (!numbers.includes(i)) answer += i;
+    }
+
     return answer;
 }
