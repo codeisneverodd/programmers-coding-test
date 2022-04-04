406024630fe71be6164da00766f1088448cff46d
jaewon <jaewon1676@daum.net>
2022-04-04 14:36:21 +0900

    Update 히샤드 수.js

--- level-1/히샤드-수.js
+++ level-1/히샤드-수.js
@@ -23,3 +23,15 @@ function solution(x) {
   // x가 각 자릿수의 합으로 나누어떨어지는지 여부 반환
   return x % sum_of_digits == 0;
 }
+
+//정답 3 - jaewon1676
+function solution(x) {
+  let sum = 0;
+  let arr = String(x).split(''); // 숫자를 하나씩 분리한다. 
+
+  for(var i=0; i<arr.length; i++) {
+      sum += Number(arr[i]); // 각 숫자를 더해준다.
+  }
+
+return (x % sum == 0) ? true : false; // 자릿수의 합으로 x가 나누어지면 하샤드 수
+}
