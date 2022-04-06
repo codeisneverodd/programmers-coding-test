a52453c18f9e1ffb489d4680d6d02818e4b8c1d2
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-04-07 08:21:19 +0900

    Update 최대공약수와-최소공배수.js

--- level-1/최대공약수와-최소공배수.js
+++ level-1/최대공약수와-최소공배수.js
@@ -12,3 +12,13 @@ const greatestCommonDivisor = (a, b) => {
     if (b === 0) return a
     else return greatestCommonDivisor(b, a % b)
 }
+
+//정답 2 - chaerin-dev
+function solution(n, m) {
+  // 최대공약수 구하기
+  let gcd = (n, m) => (n % m === 0 ? m : gcd(m, n % m));
+  // 최소공배수 구하기
+  let lcm = (n, m) => (n * m) / gcd(n, m);
+  // 결과 출력
+  return [gcd(n, m), lcm(n, m)];
+}
