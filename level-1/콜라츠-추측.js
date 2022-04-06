3635bab4df4871526db00be8aaf1b0c5bd42f3e0
Bard <prove.ability00@gmail.com>
2022-04-06 15:17:57 +0900

    conflict file update

--- level-1/콜라츠-추측.js
+++ level-1/콜라츠-추측.js
@@ -29,19 +29,20 @@ function solution(num) {
             num++;
         }
     }
-    // 작업을 반복한 횟수 반환
-    return cnt;
+  }
+  // 작업을 반복한 횟수 반환
+  return cnt;
 }
 
 //정답 3 - jaewon1676
 function solution(num) {
-    var answer = 0;
-    // num이 1이 아니고, answer이 500이 아니면 진행.
-    // 두 가지 조건중 하나라도 맞지 않으면 while문 종료.
-    while (num != 1 && answer != 500) {
-        //짝수면, 2로 나눠주고 홀수면 3을 곱하고 1을 더해준다.
-        num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
-        answer++;
-    }
-    return num == 1 ? answer : -1;
+  var answer = 0;
+  // num이 1이 아니고, answer이 500이 아니면 진행.
+  // 두 가지 조건중 하나라도 맞지 않으면 while문 종료.
+  while(num != 1 && answer != 500){ 
+      //짝수면, 2로 나눠주고 홀수면 3을 곱하고 1을 더해준다.
+      (num % 2 == 0 ? num = num / 2 : num = num * 3 + 1)
+      answer++;
+  }
+  return num == 1 ? answer : -1
 }
