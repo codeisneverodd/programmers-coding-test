629c4ae88aae65df22618dc42cbde4f1c83b32e6
yongchanson <syc9430@gmail.com>
2022-04-07 02:25:34 +0900

    [솔루션 추가]약수의-합

--- level-1/약수의-합.js
+++ level-1/약수의-합.js
@@ -6,3 +6,22 @@ function solution(n) {
     for (let divisor = 1; divisor <= n; divisor++) if (n % divisor === 0) answer += divisor
     return answer;
 }
+
+//정답 2 - yongchanson
+function solution(n) {
+  var answer = 0;
+  let i;
+  for (i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      if (i * i == n) {
+        //i의 제곱이 n인경우 처리
+        answer += i;
+      } else {
+        answer += i;
+        answer += n / i;
+      }
+    }
+  }
+
+  return answer;
+}
