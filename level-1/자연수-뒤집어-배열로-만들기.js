aa8db35ebda023f08c3a55215072cb31ca3fd067
jaewon <jaewon1676@daum.net>
2022-04-11 16:41:01 +0900

    Add 220411 자연수-뒤집어-배열로 만들기.js 2개의 풀이 추가

--- level-1/자연수-뒤집어-배열로-만들기.js
+++ level-1/자연수-뒤집어-배열로-만들기.js
@@ -18,3 +18,22 @@ function solution(n) {
     .reverse()
     .map((item) => parseInt(item));
 }
+
+//정답 3 - jaewon1676
+function solution(n) {
+    var answer = [];
+    // 정수형 => 문자형 => 하나씩 잘라서 뒤집어서 모아준다.
+    n = n.toString().split('').reverse().join('')
+    for (let i=0; i<n.length; i++){
+        answer.push(parseInt(n[i])) // n은 지금 문자형이기때문에 Push 하기전에 정수형으로 바꿔줌
+        // answer.push(n[i]) 로 해주면 현재 n[i]는 문자형이기때문에 결과는 ["5","4","3","2","1"] 가 됩니다.
+        // answer(parseInt(n[i])) 로 해준다면 n[i]는 문자형 => 정수형으로 변하기 때문에 결과는 [5, 4, 3, 2, 1] 가 됩니다.
+        // 문자형과 정수형의 차이는 숫자에 큰 따옴표가 있느냐 없느냐의 차이입니다.
+    }
+    return answer
+  }
+  
+  //정답 4 - jaewon1676
+  function solution(n) {
+    return n = n.toString().split('').reverse().map(o => o = parseInt(o));
+  }
