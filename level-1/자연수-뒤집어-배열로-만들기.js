719a2f4542e409a79d80ebab5efb4336b0c2c2ca
Bard <prove.ability00@gmail.com>
2022-04-12 16:36:20 +0900

    Add 자연수-뒤집어-배열로-만들기.js

--- level-1/자연수-뒤집어-배열로-만들기.js
+++ level-1/자연수-뒤집어-배열로-만들기.js
@@ -37,3 +37,13 @@ function solution(n) {
   function solution(n) {
     return n = n.toString().split('').reverse().map(o => o = parseInt(o));
   }
+
+
+// 정답 5 - prove-ability
+function solution(n) {
+    // 1. 문자열 변환
+    // 2. 배열로 변환
+    // 3. 배열 반전
+    // 4. 정수로 변환
+    return n.toString().split("").reverse().map((v) => parseInt(v));
+}
