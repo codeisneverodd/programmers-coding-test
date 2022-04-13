fb7d1048aaa9dd1f57b97260014d761bc33c4cec
Bard <prove.ability00@gmail.com>
2022-04-13 12:15:46 +0900

    Add 히샤드-수.js

--- level-1/히샤드-수.js
+++ level-1/히샤드-수.js
@@ -35,3 +35,13 @@ function solution(x) {
 
 return (x % sum == 0) ? true : false; // 자릿수의 합으로 x가 나누어지면 하샤드 수
 }
+
+//정답 4 - prove-ability
+function solution(x) {
+  var answer = true;
+  // 모든 자릿수의 합을 구한다
+  const sum = x.toString().split("").reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+  // x가 자릿수의 합으로 나누어떨어지지 않는다면 false 반환
+  if(x % sum !== 0) return false
+  return answer;
+}
