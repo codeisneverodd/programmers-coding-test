c95e48f8d011c31b1be3cbf382047c8663f8a666
Bard <prove.ability00@gmail.com>
2022-04-19 19:19:34 +0900

    Add 두-개-뽑아서-더하기.js

--- level-1/두-개-뽑아서-더하기.js
+++ level-1/두-개-뽑아서-더하기.js
@@ -13,3 +13,16 @@ function solution(numbers) {
     answer.sort((a, b) => a - b)
     return answer;
 }
+
+//정답 2 - prove-ability
+function solution(numbers) {
+    const answer = [];
+    
+    for(let i = 0, len = numbers.length; i < len; i++) {
+        for(let j = i + 1, len = numbers.length; j < len; j++) {
+            if(!answer.includes(numbers[i] + numbers[j])) answer.push(numbers[i] + numbers[j])
+        }
+    }
+    
+    return answer.sort((a, b) => a - b);
+}
