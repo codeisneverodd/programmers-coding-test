cdad365271c467e3130d38fe562b3c5786258b31
Bard <prove.ability00@gmail.com>
2022-04-19 19:20:22 +0900

    Add 문자열-내림차순으로-배치하기.js

--- level-1/문자열-내림차순으로-배치하기.js
+++ level-1/문자열-내림차순으로-배치하기.js
@@ -21,3 +21,8 @@ function solution(s) {
 function solution(s) {
   return s.split("").sort((a, b) => (a < b ? 1 : -1)).join("");
 }
+
+//정답4 - prove-ability
+function solution(s) {
+  return s.split("").sort().reverse().join("")
+}
