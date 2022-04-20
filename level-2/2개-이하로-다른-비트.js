d2ababc6e8cce9477e7fda2dea86070670422d7b
leesky <leehaneul0623@gmail.com>
2022-04-20 10:39:56 +0900

    Create: level2 - 2개 이하로 다른 비트, 생성 및 풀이 추가

---
+++ level-2/2개-이하로-다른-비트.js
@@ -0,0 +1,17 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//완벽한 정답이 아닙니다.
+//정답 1 - le2sky
+function solution(n) {
+  let answer = [];
+  numbers.forEach((num) => {
+    if (num % 2 == 0) answer.push(num + 1);
+    else {
+      let binary = ["0", ...num.toString(2)];
+      let last = binary.lastIndexOf("0");
+      binary[last] = "1";
+      binary[last + 1] = "0";
+      answer.push(parseInt(binary.join(""), 2));
+    }
+  });
+  return answer;
+}
