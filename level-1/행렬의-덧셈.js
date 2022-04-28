7de410f544ff9751297e07e4e627949e573bd29e
yongchanson <syc9430@gmail.com>
2022-04-28 22:43:31 +0900

    Update 행렬의-덧셈.js

--- level-1/행렬의-덧셈.js
+++ level-1/행렬의-덧셈.js
@@ -35,3 +35,16 @@ function solution(arr1, arr2) {
   }
   return answer;
 }
+
+//정답 4 - yongchanson
+function solution(A, B) {
+  let answer = [];
+
+  for (let i = 0; i < A.length; i++) {
+    answer[i] = [];
+    for (let j = 0; j < A[0].length; j++) {
+      answer[i][j] = A[i][j] + B[i][j];
+    }
+  }
+  return answer;
+}
