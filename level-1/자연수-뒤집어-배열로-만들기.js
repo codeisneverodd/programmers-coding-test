3c82af60266385d7211b4f5bbf3bfa6f193d0e8c
yongchanson <syc9430@gmail.com>
2022-04-28 17:19:28 +0900

    Update 자연수-뒤집어-배열로-만들기.js

--- level-1/자연수-뒤집어-배열로-만들기.js
+++ level-1/자연수-뒤집어-배열로-만들기.js
@@ -59,7 +59,7 @@ function solution(n) {
   let answer = [];
   let string = n.toString();
   for (let i = string.length - 1; i >= 0; i--) {
-    let slice = string.substr(i, 1) * 1;
+    let slice = string.slice(i, i + 1) * 1;
     answer.push(slice);
   }
   return answer;
