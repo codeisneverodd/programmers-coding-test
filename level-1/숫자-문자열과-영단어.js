191d7866b842b45bd6f6345d5f73ce924a5fd70f
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-04-29 20:21:03 +0900

    Update 숫자-문자열과-영단어.js

--- level-1/숫자-문자열과-영단어.js
+++ level-1/숫자-문자열과-영단어.js
@@ -62,3 +62,19 @@ function solution(s) {
   answer = Number(s);
   return answer;
 }
+
+//정답 4 - chaerin-dev
+function solution(s) {
+  s = s
+    .replace(/zero/g, 0)
+    .replace(/one/g, 1)
+    .replace(/two/g, 2)
+    .replace(/three/g, 3)
+    .replace(/four/g, 4)
+    .replace(/five/g, 5)
+    .replace(/six/g, 6)
+    .replace(/seven/g, 7)
+    .replace(/eight/g, 8)
+    .replace(/nine/g, 9);
+  return parseInt(s);
+}
