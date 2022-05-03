dffb0ac265463f0c12eefc4bb980247b46bce1b0
Bard <prove.ability00@gmail.com>
2022-05-03 22:56:51 +0900

    Update 가장-큰-수.js

--- level-2/가장-큰-수.js
+++ level-2/가장-큰-수.js
@@ -23,3 +23,32 @@ function solution(numbers) {
   
     return stringNum[0] === '0' ? '0' : stringNum.join('');
 }
+
+//완벽한 정답이 아닙니다.
+// 정답 3 - prove-ability
+function solution(numbers) {
+  if(numbers.every(v => v === 0)) return "0";
+  return numbers.sort((a, b) => {
+      if(a === b) return 0;
+      let stringA = a.toString(10), stringB = b.toString(10);
+      if(stringA[0] === stringB[0]) {
+          let aIndex = 1, bIndex = 1;
+          while(true) {
+              if(!stringA[aIndex]) --aIndex;
+              if(!stringB[bIndex]) --bIndex;
+              if(stringA[aIndex] === stringB[bIndex]) {
+                  aIndex++, bIndex++;
+                  continue;
+              }
+              if(stringA[aIndex] < stringB[bIndex]) return 1;
+              else return -1;
+          }            
+      } 
+      return stringB[0] - stringA[0]
+  }).join("");
+}
+
+// 정답 4 - prove-ability
+function solution(numbers) {
+  return numbers.every(v => v === 0) ? "0" : numbers.map(v => v.toString(10)).sort((a,b) => (b+a) - (a+b)).join("");
+}
