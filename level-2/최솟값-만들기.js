a7904853736900c1108d0921c16093e4bc36dd76
yongchanson <syc9430@gmail.com>
2022-05-26 01:29:31 +0900

    Add 최솟값-만들기.js

--- level-2/최솟값-만들기.js
+++ level-2/최솟값-만들기.js
@@ -21,3 +21,10 @@ function solution(A,B){
 
   return sum;
 }
+
+//정답 3 - yongchanson
+function solution(A, B) {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+  return A.reduce((t, n, i) => t + n * B[i], 0);
+}
