80613455147aa2526f1c51ffd3a3e55bda7babef
chaerin-dev <chaerin.dev@gmail.com>
2022-07-19 19:22:00 +0900

    solution 위장

--- level-2/위장.js
+++ level-2/위장.js
@@ -67,3 +67,23 @@ function solution(clothes) {
 3. obj에 해당 키가 있으면 해당 키의 값을 불러오고 1을 더해줌.
 4. for in 구문으로 obj의 키를 반복하여 불러오고 해당 값을 answer에 곱해줌
 5. 최소한 1가지 이상의 옷을 입기 떄문에 옷을 입지 않은 경우 -1로 제외. */
+
+//정답 5 - chaerin-dev
+function solution(clothes) {
+  // 각 카테고리별 의상의 수를 clothsObj 객체에 저장
+  const clothsObj = {};
+  clothes.forEach((cloth) =>
+    clothsObj[cloth[1]] ? clothsObj[cloth[1]]++ : (clothsObj[cloth[1]] = 1)
+  );
+
+  // 어떤 카테고리의 옷의 개수가 n개라면, 스파이는 해당 카테고리에 대해 n+1가지 선택권을 가짐
+  // (옷을 입지 않거나, 1번 옷을 입거나, 2번 옷을 입거나, ..., n번 옷을 입거나)
+  // 즉, (각 카테고리의 옷의 개수 + 1)를 answer에 차례로 곱해주면 됨
+  let answer = 1;
+  for (let key of Object.keys(clothsObj)) {
+    answer *= clothsObj[key] + 1;
+  }
+
+  // 스파이는 하루에 최소 한 개의 의상은 입어야 하므로 아무것도 입지 않는 경우 빼기
+  return answer - 1;
+}
