24243d8a331324a59731e47f58f8730db724bb7c
codeisneverodd <codeisneverodd@gmail.com>
2022-07-20 10:42:41 +0900

    [17주차] 문제 풀이 추가 (#69)
    * solution: level 2 / 하노이 탑 풀이 추가
    
    * solution: 모음사전
    
    * solution 체육복
    
    * solution 모의고사
    
    * solution 위장
    
    * solution 튜플
    
    Co-authored-by: chaerin-dev <chaerin.dev@gmail.com>

--- level-1/체육복.js
+++ level-1/체육복.js
@@ -88,3 +88,40 @@ function solution(n, lost, reserve) {
   //그리디
   // lost 배열과 reserve 배열을 순회하여 체육복을 추가, 제거 해줍니다.
   // 그 후에 최종적으로 i부터 n까지 for문을 순회하며 i번쨰 학생과 i+1번째의 학생이 가진 체육복 수를 비교하여 빌려 줄 수 있는지, 빌려줄 수 없는지 확인 합니다.
+
+//정답 4 - chaerin-dev
+function solution(n, lost, reserve) {
+  // 학생들의 체육복 개수를 저장할 배열 students
+  // 학생의 index가 1번부터 시작하므로 배열의 길이를 n+1로 설정
+  // 0번 학생은 실제로는 없지만 초기값을 1로 설정하면 이후 연산에 영향을 미치지 않음
+  const students = Array.from({ length: n + 1 }, () => 1);
+
+  // 체육복 분실/여분 정보 students 배열에 반영
+  lost.forEach((lostStudent) => students[lostStudent]--);
+  reserve.forEach((reserveStudent) => students[reserveStudent]++);
+
+  // 체육복이 없어 체육 수업을 들을 수 없는 학생 수를 저장할 변수 cnt
+  let cnt = 0;
+  students.forEach((student, i) => {
+    // 현재 인덱스의 학생이 체육복이 없다면
+    if (student === 0) {
+      // 바로 앞 학생이 체육복 여분이 있다면
+      if (students[i - 1] === 2) {
+        // 바로 앞 학생에게 체육복 빌리기
+        students[i - 1]--;
+        student++;
+      }
+      // 바로 앞 학생에게 체육복을 빌리지 못했고, 바로 뒤 학생이 체육복 여분이 있다면
+      else if (students[i + 1] === 2) {
+        // 바로 뒤 학생에게 체육복 빌리기
+        students[i + 1]--;
+        student++;
+      }
+      // 바로 앞 학생과 바로 뒤 학생 모두에게 체육복을 빌리지 못했다면 체육 수업을 들을 수 없음
+      else cnt++;
+    }
+  });
+
+  // 체육 수업을 들을 수 있는 학생 수 = 전체 학생 수 - 체육 수업을 들을 수 없는 학생 수
+  return n - cnt;
+}
