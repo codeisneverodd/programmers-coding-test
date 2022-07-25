8938fca9f0de91b554c407f81518732cc7685394
Jaeyeong son <nardi0302@gmail.com>
2022-07-25 20:38:10 +0900

    fix: 최소힙 클래스 바깥 분리&파일 컨벤션 적용

--- level-2/배달.js
+++ level-2/배달.js
@@ -1,79 +1,81 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//완벽한 정답이 아닙니다.
+
 //정답 1 - pereng11
 // 다익스트라 + 최소힙 O( N * logN ) 
+    // [목적지, 거리] 노드를 값으로 가지는, 거리에 대한 최소힙 
+class MinHeap{
+    constructor ()
+    {
+      this.heap = [ null ];
+    }
+    // 맨 끝에 노드를 삽입 후 위로 올라가면서 정렬
+    push ( val )
+    {
+          this.heap.push(val);
+          let childIdx = this.heap.length-1;
+          let parentIdx = Math.floor(childIdx / 2);
+          while(parentIdx > 0 && this.heap[parentIdx][1] > this.heap[childIdx][1]){
+            this.swap( childIdx, parentIdx );
+            childIdx = parentIdx;
+            parentIdx = Math.floor(childIdx / 2);
+          }
+    }
+    pop ()
+    {
+      if ( this.heap.length === 1 )
+      {
+        return undefined;
+      }
+      // 최소값은 빼두었다가 리턴하고, 가장 끝 값을 맨 위로 가져와 아래로 내려가면서 정렬
+      const minNode = this.heap[ 1 ];
+      this.heap[ 1 ] = this.heap[ this.heap.length - 1 ];
+      this.heap.pop();
+      let parentIdx = 1;
+      let leftChildIdx = 2;
+      let rightChildIdx = 3;
+      while ( parentIdx < this.heap.length )
+      {
+        // 자식이 없는 경우 
+        if ( !this.heap[ leftChildIdx ] )
+        {
+          break;
+        } // 왼쪽 자식만 있는 경우
+        else if ( !this.heap[ rightChildIdx ] )
+        {
+          if ( this.heap[ parentIdx ][ 1 ] > this.heap[ leftChildIdx ][ 1 ] )
+          {
+            this.swap( parentIdx, leftChildIdx );
+          }
+          break;
+          // 둘 중 하나가 부모보다 작을 때, 더 작은 쪽으로 정렬
+        } else if ( this.heap[ parentIdx ][ 1 ] > this.heap[ leftChildIdx ][ 1 ] || this.heap[ parentIdx ][ 1 ] > this.heap[ rightChildIdx ][ 1 ] )
+        {
+          const minChildIdx = this.heap[ leftChildIdx ][ 1 ] < this.heap[ rightChildIdx ][ 1 ] ? leftChildIdx : rightChildIdx;
+          this.swap( parentIdx, minChildIdx );
+          parentIdx = minChildIdx;
+          leftChildIdx = parentIdx * 2
+          rightChildIdx = parentIdx * 2 + 1;
+        } else
+        {
+          // 끝까지 내려가지 않았더라도 부모가 가장 작으면 정렬 중지
+          break;
+        }
+      }
+      return minNode;
+    }
+    swap ( idx1, idx2 )
+    {
+        [ this.heap[ idx1 ], this.heap[ idx2 ] ] = [ this.heap[ idx2 ], this.heap[ idx1 ] ];
+    }
+    length ()
+    {
+      return this.heap.length;
+    }
+}
+  
 function solution ( N, road, K )
 {
-    // [목적지, 거리] 노드를 값으로 가지는, 거리에 대한 최소힙 
-  class MinHeap{
-      constructor ()
-      {
-        this.heap = [ null ];
-      }
-      // 맨 끝에 노드를 삽입 후 위로 올라가면서 정렬
-      push ( val )
-      {
-            this.heap.push(val);
-            let childIdx = this.heap.length-1;
-            let parentIdx = Math.floor(childIdx / 2);
-            while(parentIdx > 0 && this.heap[parentIdx][1] > this.heap[childIdx][1]){
-              this.swap( childIdx, parentIdx );
-              childIdx = parentIdx;
-              parentIdx = Math.floor(childIdx / 2);
-            }
-      }
-      pop ()
-      {
-        if ( this.heap.length === 1 )
-        {
-          return undefined;
-        }
-        // 최소값은 빼두었다가 리턴하고, 가장 끝 값을 맨 위로 가져와 아래로 내려가면서 정렬
-        const minNode = this.heap[ 1 ];
-        this.heap[ 1 ] = this.heap[ this.heap.length - 1 ];
-        this.heap.pop();
-        let parentIdx = 1;
-        let leftChildIdx = 2;
-        let rightChildIdx = 3;
-        while ( parentIdx < this.heap.length )
-        {
-          // 자식이 없는 경우 
-          if ( !this.heap[ leftChildIdx ] )
-          {
-            break;
-          } // 왼쪽 자식만 있는 경우
-          else if ( !this.heap[ rightChildIdx ] )
-          {
-            if ( this.heap[ parentIdx ][ 1 ] > this.heap[ leftChildIdx ][ 1 ] )
-            {
-              this.swap( parentIdx, leftChildIdx );
-            }
-            break;
-            // 둘 중 하나가 부모보다 작을 때, 더 작은 쪽으로 정렬
-          } else if ( this.heap[ parentIdx ][ 1 ] > this.heap[ leftChildIdx ][ 1 ] || this.heap[ parentIdx ][ 1 ] > this.heap[ rightChildIdx ][ 1 ] )
-          {
-            const minChildIdx = this.heap[ leftChildIdx ][ 1 ] < this.heap[ rightChildIdx ][ 1 ] ? leftChildIdx : rightChildIdx;
-            this.swap( parentIdx, minChildIdx );
-            parentIdx = minChildIdx;
-            leftChildIdx = parentIdx * 2
-            rightChildIdx = parentIdx * 2 + 1;
-          } else
-          {
-            // 끝까지 내려가지 않았더라도 부모가 가장 작으면 정렬 중지
-            break;
-          }
-        }
-        return minNode;
-      }
-      swap ( idx1, idx2 )
-      {
-          [ this.heap[ idx1 ], this.heap[ idx2 ] ] = [ this.heap[ idx2 ], this.heap[ idx1 ] ];
-      }
-      length ()
-      {
-        return this.heap.length;
-      }
-  }
-  
-
   const roadsTable = {}; //전체 도로 정보
   
   // 도로 정보 초기화 roadTable[시작점] = [목적지, 거리] 배열
@@ -136,7 +138,6 @@ function solution ( N, road, K )
 //다익스트라 + 선형탐색
 function solution ( N, road, K )
 {  
-
   const roadsTable = {}; //전체 도로 정보
   
   // 도로 정보 초기화 roadTable[시작점] = [목적지, 거리] 배열
