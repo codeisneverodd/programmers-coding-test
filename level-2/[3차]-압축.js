cbe70eff4b36891f0f61a7a7d559a0e22bfdf811
codeisneverodd <codeisneverodd@gmail.com>
2022-09-01 22:40:33 +0900

    feat: 2 / 압축 / 새로운 문제

---
+++ level-2/[3차]-압축.js
@@ -0,0 +1,33 @@
+//https://github.com/codeisneverodd/programmers-coding-test
+//더 좋은 풀이가 존재할 수 있습니다.
+//정답 1 - codeisneverodd
+function solution(msg) {
+  const outputs = [];
+  const dict = ['@', ...Array.from(Array(26), (_, i) => String.fromCharCode(65 + i))];
+  let [start, end] = [0, 1];
+
+  const doesDictHave = (start, end) => dict.includes(msg.substring(start, end));
+
+  const getInput = (start, end) => {
+    if (end + 1 > msg.length || !doesDictHave(start, end + 1)) return [start, end];
+    return getInput(start, end + 1);
+  };
+
+  const addToDict = (start, end) => {
+    if (doesDictHave(start, end)) return;
+    dict.push(msg.substring(start, end));
+  };
+
+  const addToOutputs = (start, end) => {
+    if (!doesDictHave(start, end)) return;
+    outputs.push(dict.indexOf(msg.substring(start, end)));
+  };
+
+  while (start < msg.length) {
+    [start, end] = getInput(start, end);
+    addToDict(start, end + 1);
+    addToOutputs(start, end);
+    start = end;
+  }
+  return outputs;
+}
