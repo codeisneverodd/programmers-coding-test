de5a0c39a17ec135d78a5d091fbae7befc80ccb9
codeisneverodd <codeisneverodd@gmail.com>
2022-09-07 20:15:04 +0900

    feat: 레벨 이동 및 전체 문제 수 변경

--- level-3/이중우선순위큐.js
+++ level-3/이중우선순위큐.js
@@ -4,28 +4,28 @@
 function solution(operations) {
   var answer = [];
   for (let i = 0; i < operations.length; i++) {
-      
-      // 숫자 삽입
-      if (operations[i][0] == 'I') {
-          let m = operations[i].substring(2, operations[i].length)
-          answer.push(m)
-      }
-      // if 최댓값 삭제
-      else if (operations[i][0] == 'D' && operations[i][2] == '1' && operations.length > 0) {
-          answer.pop()
-      }
-      // if 최솟값 삭제
-      else if (operations[i][0] == 'D' && operations[i][2] == '-' && operations[i][3] == '1' && operations.length > 0){
-          
-          answer.shift()            
-      }
-      
-      answer.sort((a, b) => {return a - b});
+    // 숫자 삽입
+    if (operations[i][0] == 'I') {
+      let m = operations[i].substring(2, operations[i].length);
+      answer.push(m);
+    }
+    // if 최댓값 삭제
+    else if (operations[i][0] == 'D' && operations[i][2] == '1' && operations.length > 0) {
+      answer.pop();
+    }
+    // if 최솟값 삭제
+    else if (operations[i][0] == 'D' && operations[i][2] == '-' && operations[i][3] == '1' && operations.length > 0) {
+      answer.shift();
+    }
+
+    answer.sort((a, b) => {
+      return a - b;
+    });
   }
-  if (answer.length == 0) return [0, 0]
+  if (answer.length == 0) return [0, 0];
   else {
-      return [parseInt(answer.pop()), parseInt(answer.shift())];
-  }     
+    return [parseInt(answer.pop()), parseInt(answer.shift())];
+  }
 }
 /* 풀이 과정
 1. 연산 처리를 구별하기 위해 배열의 0번째 자리, 2번째 자리에 있는 등을 비교하여 조건에 따른 명령을 실행한다.
