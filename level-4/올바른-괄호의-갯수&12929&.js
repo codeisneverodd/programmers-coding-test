63919d2a7145ad6f04346f3919655e58cfb19ca9
Lee Soo Hyuk <30382481+cg10036@users.noreply.github.com>
2022-10-11 09:04:21 +0900

    Update level-4/올바른-괄호의-갯수&12929&.js
    Co-authored-by: codeisneverodd <codeisneverodd@gmail.com>

--- level-4/올바른-괄호의-갯수&12929&.js
+++ level-4/올바른-괄호의-갯수&12929&.js
@@ -1,10 +1,7 @@
 //https://github.com/codeisneverodd/programmers-coding-test
 //완벽한 정답이 아닙니다.
 //정답 1 - cg10036
-function fact(n) {
-    return n ? BigInt(n) * fact(n - 1) : 1n;
-}
-
 function solution(n) {
+    const fact = n => n ? BigInt(n) * fact(n - 1) : 1n;
     return fact(n * 2) / (fact(n) * fact(n + 1));
 }
